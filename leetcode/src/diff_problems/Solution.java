1e49466e00eda263191e575495b8f0cd3303642c
Unknown <gthor10@gmail.com>
2018-08-03 14:17:13 -0700

    daily problems - estimate pi with monte carlo method
    estimate pi with monte carlo method (random generated input)

--- leetcode/src/diff_problems/Solution.java
+++ leetcode/src/diff_problems/Solution.java
@@ -1,6 +1,7 @@
 package diff_problems;
 
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 import java.util.Stack;
 
@@ -230,6 +231,118 @@ public class Solution {
         return memoSols[m.length];
     }
 
+    /**
+     * This problem was asked by Amazon.
+     *
+     * There exists a staircase with N steps, and you can climb up either 1 or 2 steps at a time. Given N, write a function that returns the number of unique ways you can climb the staircase. The order of the steps matters.
+     *
+     * For example, if N is 4, then there are 5 unique ways:
+     *
+     * 1, 1, 1, 1
+     * 2, 1, 1
+     * 1, 2, 1
+     * 1, 1, 2
+     * 2, 2
+     * What if, instead of being able to climb 1 or 2 steps at a time, you could climb any number from a set of positive integers X? For example, if X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time.
+     *
+     * @param N
+     * @return
+     */
+    int getNumWays(int N) {
+        //all solution is based on idea that there is a pattern that n of steps required to climb on staircase with i stairs = steps[i-1] + steps[i-2] -
+        //sum of number of steps required to climb on staircase with i-1 and i-2 stairs
+        //base case
+        if (N == 0)
+            return 1;
+        //this is set of possible steps. When steps are fixed it's possible to
+        //avoid nested loop and address memoized data directly by index -> code looks simpler
+        int[] steps = new int[] {1, 2};
+        //cache for DP memoization
+        int[] memo = new int[N + 1];
+        memo[0] = 1;
+        for (int i = 1; i <= N; i++) {
+            int total = 0;
+            for (int j = 0; j < steps.length; j++) {
+                if (i - steps[j]>=0)
+                    total += memo[i-steps[j]];
+            }
+            memo[i] = total;
+        }
+        return memo[N];
+    }
+
+    /**
+     * This problem was asked by Amazon.
+     *
+     * Given an integer k and a string s, find the length of the longest substring that contains at most k distinct characters.
+     *
+     * For example, given s = "abcba" and k = 2, the longest substring with k distinct characters is "bcb".
+     *
+     * @param s
+     * @param k
+     * @return
+     */
+    int longestSubstringLength(String s, int k) {
+        int res = 0;
+        int[] charCounts = new int[26];
+        int left = 0;
+        int currentIterationCount = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int charIndex = s.charAt(i) - 97;
+            if (charCounts[charIndex] == 0) {
+                currentIterationCount++;
+            }
+            if (currentIterationCount <= k) {
+                charCounts[charIndex]++;
+            } else {
+                res = Math.max(res, i - left);
+                while(true) {
+                    int leftIndex = s.charAt(left) - 97;
+                    charCounts[leftIndex]--;
+                    left++;
+                    if (charCounts[leftIndex] == 0) {
+                        currentIterationCount--;
+                        break;
+                    }
+                }
+                charCounts[charIndex]++;
+            }
+        }
+        res = Math.max(res, s.length() - left);
+        return res;
+    }
+
+    /**
+     * This problem was asked by Google.
+     *
+     * The area of a circle is defined as πr^2. Estimate π to 3 decimal places using a Monte Carlo method.
+     *
+     * Hint: The basic equation of a circle is x2 + y2 = r2.
+     *
+     * @return
+     */
+    double getPi() {
+        //idea is to use math property of the circle.
+        //Let's take square 1*1, it's area is 1. Then let's inscribe a quadrant within it.
+        //all points that are within that quadrant should fit the inequity x^2+y^2 < r^2 which in this case
+        //transforms into x^2+y^2 < 1. So if we uniformly cover square with points and count how many are inside
+        //the quadrant and how many are outside, then will have area of 1/4 of the circle = pi*r^2/4 = pi/4.
+        //So pi in this case will be pi = 4*(area_of_quadrant)=4*(num_points_in_circle/total_number_of_points)
+        int numOfTries = 25000;
+        Random rand = new Random();
+        int countInCircle = 0;
+        for (int i = 0; i < numOfTries; i++) {
+            double nextX = rand.nextDouble();
+            double nextY = rand.nextDouble();
+            double distance = Math.sqrt(nextX*nextX + nextY*nextY);
+            if (distance < 1.0f)
+                countInCircle++;
+        }
+
+        double pi = (countInCircle/(double)numOfTries)*4.0;
+        return pi;
+    }
+
     public static void main(String[] args) {
         Solution obj = new Solution();
         //[3,5,1,6,2,9,8,null,null,7,4]
@@ -260,7 +373,13 @@ public class Solution {
 
         //System.out.println(obj.minEatingSpeed(new int[]{3,6,7,11}, 8));
 
-        System.out.println(obj.getNumberOfEncodings("111".toCharArray()));
-        System.out.println(obj.getNumberOfEncodings("2712211".toCharArray()));
+        //System.out.println(obj.getNumberOfEncodings("111".toCharArray()));
+        //System.out.println(obj.getNumberOfEncodings("2712211".toCharArray()));
+
+        //System.out.println(obj.getNumWays(4));
+
+        //System.out.println(obj.longestSubstringLength("abbgbbcba", 2));
+
+        System.out.println(obj.getPi());
     }
 }
