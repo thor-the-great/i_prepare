a7b8eb0693665b849d6ffc182ec95f69a4bf13db
Unknown <gthor10@gmail.com>
2018-10-03 12:25:07 -0700

    daily problems - next month, october
    - calculate num of time number is in the multiplication grid

---
+++ daily_problems/src/SolutionDailyCodingOctober2018.java
@@ -0,0 +1,101 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SolutionDailyCodingOctober2018 {
+
+    /**
+     * This problem was asked by Apple.
+     *
+     * Suppose you have a multiplication table that is N by N. That is, a 2D array where the value at the i-th row and
+     * j-th column is (i + 1) * (j + 1) (if 0-indexed) or i * j (if 1-indexed).
+     *
+     * Given integers N and X, write a function that returns the number of times X appears as a value in an N by N
+     * multiplication table.
+     *
+     * For example, given N = 6 and X = 12, you should return 4, since the multiplication table looks like this:
+     *
+     * | 1 | 2 | 3 | 4 | 5 | 6 |
+     *
+     * | 2 | 4 | 6 | 8 | 10 | 12 |
+     *
+     * | 3 | 6 | 9 | 12 | 15 | 18 |
+     *
+     * | 4 | 8 | 12 | 16 | 20 | 24 |
+     *
+     * | 5 | 10 | 15 | 20 | 25 | 30 |
+     *
+     * | 6 | 12 | 18 | 24 | 30 | 36 |
+     *
+     * And there are 4 12's in the table.
+     *
+     * @param N
+     * @param X
+     * @return
+     *
+     */
+    int numInGrid(int N, int X) {
+        int count = 0;
+        for (int i = 1; i <= N; i++) {
+            if (X % i == 0 && X / i <= N)
+                count++;
+        }
+        return count;
+        //return seiveBasedComplex(N, X);
+    }
+
+    private int seiveBasedComplex(int N, int X) {
+        //calculate all prime numbers using seive method
+        boolean[] seive = new boolean[X + 1];
+        Arrays.fill(seive, true);
+        //primes will be marked as true, then eliminate not primes
+        for (int i = 2; i*i < X; i++) {
+            if (seive[i]) {
+                for (int j = i*2; j < X; j += i) {
+                    seive[j] = false;
+                }
+            }
+        }
+        //now iterate over primes, divisors are factors of primes
+        int numOfDivisors = 1;
+        Set<Integer> usedNums = new HashSet();
+        int n = X;
+        for (int p = 2; p <= N; p++) {
+            if (seive[p]) {
+                int count = 0;
+                if (X % p == 0) {
+                    usedNums.add(p);
+                    while (n % p == 0 ) {
+                        n = n / p;
+                        if (!usedNums.contains(n) && n <= N && X/n <= N) {
+                            count++;
+                            usedNums.add(n);
+                        }
+                    }
+                    if (count > 0)
+                        numOfDivisors *= count;
+                }
+            }
+        }
+        int result = 2*numOfDivisors;
+        //check that it's not in the table diagonal
+        double sqrt = Math.sqrt(X);
+        if (sqrt - Math.floor(sqrt) == 0 && result > 0) {
+            result -= 1;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        SolutionDailyCodingOctober2018 obj = new SolutionDailyCodingOctober2018();
+
+        System.out.println("------ find number of times number X is in grid of multiplication N by N ---------");
+        System.out.println("N=6, X=12; " + obj.numInGrid(6, 12));
+        System.out.println("N=6, X=6; " + obj.numInGrid(6, 6));
+        System.out.println("N=6, X=8; " + obj.numInGrid(6, 8));
+        System.out.println("N=6, X=30; " + obj.numInGrid(6, 30));
+        System.out.println("N=8, X=16; " + obj.numInGrid(8, 16));
+        System.out.println("N=10, X=24; " + obj.numInGrid(10, 24));
+        System.out.println("N=15, X=20; " + obj.numInGrid(15, 20));
+    }
+}
