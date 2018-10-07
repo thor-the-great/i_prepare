383f0af4a262973788fd9c2dee8d882fc1c3eb70
Unknown <gthor10@gmail.com>
2018-10-07 13:40:16 -0700

    leetcode cp contest
    - max sum in circular sub-array

--- leetcode/src/diff_problems/Solution.java
+++ leetcode/src/diff_problems/Solution.java
@@ -235,6 +235,76 @@ public class Solution {
         return new String(newString);
     }
 
+    /**
+     * 918. Maximum Sum Circular Subarray
+     *
+     * Given a circular array C of integers represented by A, find the maximum possible sum of a non-empty subarray of C.
+     *
+     * Here, a circular array means the end of the array connects to the beginning of the array.  (Formally, C[i] = A[i]
+     * when 0 <= i < A.length, and C[i+A.length] = C[i] when i >= 0.)
+     *
+     * Also, a subarray may only include each element of the fixed buffer A at most once.  (Formally, for a subarray
+     * C[i], C[i+1], ..., C[j], there does not exist i <= k1, k2 <= j with k1 % A.length = k2 % A.length.)
+     *
+     *
+     *
+     * Example 1:
+     *
+     * Input: [1,-2,3,-2]
+     * Output: 3
+     * Explanation: Subarray [3] has maximum sum 3
+     * Example 2:
+     *
+     * Input: [5,-3,5]
+     * Output: 10
+     * Explanation: Subarray [5,5] has maximum sum 5 + 5 = 10
+     * Example 3:
+     *
+     * Input: [3,-1,2,-1]
+     * Output: 4
+     * Explanation: Subarray [2,-1,3] has maximum sum 2 + (-1) + 3 = 4
+     * Example 4:
+     *
+     * Input: [3,-2,2,-3]
+     * Output: 3
+     * Explanation: Subarray [3] and [3,-2,2] both have maximum sum 3
+     * Example 5:
+     *
+     * Input: [-2,-3,-1]
+     * Output: -1
+     * Explanation: Subarray [-1] has maximum sum -1
+     *
+     *
+     * Note:
+     *
+     * -30000 <= A[i] <= 30000
+     * 1 <= A.length <= 30000
+     *
+     * @param A
+     * @return
+     */
+    public int maxSumCircularSubarray(int[] A) {
+        int maxDirectSum = maxKadane(A);
+        int maxWrapSum = 0;
+        int[] negated = new int[A.length];
+        for (int i = 0; i < A.length; i++) {
+            maxWrapSum += A[i];
+            negated[i] = -A[i];
+        }
+        maxWrapSum = maxWrapSum + maxKadane(negated);
+        return maxDirectSum < 0 ? Math.min(maxDirectSum, maxWrapSum) : Math.max(maxDirectSum, maxWrapSum);
+    }
+
+    int maxKadane(int[] A){
+        int max_to_here = A[0];
+        int max = A[0];
+        for (int i = 1; i < A.length; i++) {
+            max_to_here = Math.max(A[i], max_to_here + A[i]);
+            max = Math.max(max, max_to_here);
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
         Solution obj = new Solution();
         //[3,5,1,6,2,9,8,null,null,7,4]
@@ -286,5 +356,13 @@ public class Solution {
         res = obj.reverseOnlyLetters("a-bC-dEf-ghIj");
         golden = "j-Ih-gfE-dCba";
         System.out.println(res + " ? " + golden +" " + res.equals(golden));
+
+        System.out.println("---- max sum of circular subarray ------");
+        System.out.println(obj.maxSumCircularSubarray(new int[]{ 3, -3, 1}));//4
+        System.out.println(obj.maxSumCircularSubarray(new int[]{-2,-3,-1}));//-1
+        System.out.println(obj.maxSumCircularSubarray(new int[] {1,-2,3,-2})); //3
+        System.out.println(obj.maxSumCircularSubarray(new int[] {5,-3,5})); //10
+        System.out.println(obj.maxSumCircularSubarray(new int[] {3,-1,2,-1})); //4
+        System.out.println(obj.maxSumCircularSubarray(new int[] {3,-2,2,-3})); //3
     }
 }
