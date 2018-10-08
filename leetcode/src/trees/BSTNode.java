c23d6a90b702ee961a507334699386ac1e59c085
Unknown <gthor10@gmail.com>
2018-10-08 09:57:04 -0700

    general algos
    tree traversals - added class to generic, adjust some code in other modules to be public accessible

--- leetcode/src/trees/BSTNode.java
+++ leetcode/src/trees/BSTNode.java
@@ -5,7 +5,7 @@ public class BSTNode {
     public BSTNode right;
     public BSTNode left;
 
-    BSTNode (int val, BSTNode right, BSTNode left) {
+    public BSTNode (int val, BSTNode right, BSTNode left) {
         this.val = val;
         this.right = right;
         this.left = left;
