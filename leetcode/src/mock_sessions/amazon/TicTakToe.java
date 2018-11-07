453f48ea41bdfa25e1c138200ca72da2ccce9014
Unknown <gthor10@gmail.com>
2018-11-06 17:50:49 -0800

    leetcode amazon
    - tic tak toe design

---
+++ leetcode/src/mock_sessions/amazon/TicTakToe.java
@@ -0,0 +1,39 @@
+package mock_sessions.amazon;
+
+class TicTacToe {
+
+    /** Initialize your data structure here. */
+    int[] cols;
+    int[] rows;
+    int diag;
+    int antidiag;
+    public TicTacToe(int n) {
+        cols = new int[n];
+        rows = new int[n];
+        diag = 0;
+        antidiag = 0;
+    }
+
+    /** Player {player} makes a move at ({row}, {col}).
+     @param row The row of the board.
+     @param col The column of the board.
+     @param player The player, can be either 1 or 2.
+     @return The current winning condition, can be either:
+     0: No one wins.
+     1: Player 1 wins.
+     2: Player 2 wins. */
+    public int move(int row, int col, int player) {
+        int x = player == 1 ? 1 : -1;
+        int N = rows.length;
+        rows[row] += x;
+        cols[col] += x;
+        if (row == col)
+            diag += x;
+        if (N - row - 1 == col)
+            antidiag += x;
+        if (Math.abs(rows[row]) == N || Math.abs(cols[col]) == N || Math.abs(diag) == N || Math.abs(antidiag) == N)
+            return player;
+        else
+            return 0;
+    }
+}
