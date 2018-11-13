4b5f87dacfa8b94fe175f73977634d9860f7d9ca
Unknown <gthor10@gmail.com>
2018-11-12 22:04:44 -0800

    min sliding window - fixed, improved by changing map to array

--- leetcode/src/mock_sessions/amazon/MinSlidingWindows.java
+++ leetcode/src/mock_sessions/amazon/MinSlidingWindows.java
@@ -5,7 +5,45 @@ import java.util.Map;
 import java.util.stream.IntStream;
 
 public class MinSlidingWindows {
+
     public String minWindow(String s, String t) {
+        int[] windowCount = new int[256];
+        int[] tCount = new int[256];
+        int reqNum = 0;
+        for(char c : t.toCharArray()) {
+            if (tCount[c] == 0)
+                reqNum++;
+            tCount[c] += 1;
+        }
+        int foundCount = 0;
+        int l = 0, r = 0;
+        int[] res = new int[] {-1, 0, 0};
+        while (r < s.length()) {
+            char nextChar = s.charAt(r);
+            windowCount[nextChar] += 1;
+            if (tCount[nextChar] > 0 && tCount[nextChar] == windowCount[nextChar]) {
+                foundCount++;
+            }
+            while (l <= r && foundCount == reqNum) {
+                char leftChar = s.charAt(l);
+                if (res[0] == -1 || r - l + 1 < res[0]) {
+                    res[0] = r - l + 1;
+                    res[1] = l;
+                    res[2] = r;
+                }
+                windowCount[leftChar] -= 1;
+                if (tCount[leftChar] > 0 && windowCount[leftChar] < tCount[leftChar]) {
+                    foundCount--;
+                }
+                l++;
+            }
+            r++;
+        }
+        if (res[0] == -1) return "";
+        else return s.substring(res[1], res[2] + 1);
+    }
+
+    public String minWindowMap(String s, String t) {
         Map<Character, Integer> windowCount = new HashMap();
         Map<Character, Integer> tCount = new HashMap();
         for(char c : t.toCharArray()){
@@ -47,6 +85,6 @@ public class MinSlidingWindows {
 
     public static void main(String[] args) {
         MinSlidingWindows obj = new MinSlidingWindows();
-        System.out.println(obj.minWindow("aa", "aa"));
+        System.out.println(obj.minWindow("ADOBECODEBANC", "ABC")); //"BANC"
     }
 }
