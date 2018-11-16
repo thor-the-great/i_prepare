7e425836126fb6ff9ef4dd1e46aeab3bdb581095
Unknown <gthor10@gmail.com>
2018-11-15 17:02:50 -0800

    great refactoring done in LFU - now passes leetcode grader

--- daily_problems/src/lfu_cache/LFUCache.java
+++ daily_problems/src/lfu_cache/LFUCache.java
@@ -11,7 +11,7 @@ public class LFUCache {
     //is to have this LinkedHashMap, so it keeps insertion order automatically, so keySet() will return keys in "insertion
     //order", meaning first will be one inserted first etc. This is great for eviction logic - we just take first
     //element in iteration and remove it. Both operations are in O(1)
-    DefaultDict freqMap = new DefaultDict();
+    Map<Integer, LinkedHashMap<Integer, Integer>> freqMap = new HashMap();
     int minFreq = 0;
     int capacity;
 
@@ -28,27 +28,27 @@ public class LFUCache {
             return -1;
         }
         Node nodeVal = keyValue.get(key);
-        LinkedHashMap<Integer, Integer> deq = freqMap.get(nodeVal.freq);
+        Map<Integer, Integer> deq = getKey(nodeVal.freq);
         deq.remove(key);
         if (deq.isEmpty()) {
             freqMap.remove(nodeVal.freq);
             if (minFreq == nodeVal.freq)
                 minFreq += 1;
         }
-        Node newNodeVal = new Node(nodeVal.val, nodeVal.freq + 1);
-        keyValue.put(key, newNodeVal);
-        freqMap.get(nodeVal.freq + 1).put(key, key);
+        nodeVal.freq += 1;
+        getKey(nodeVal.freq).put(key, key);
         return nodeVal.val;
     }
 
-    public void set(int key, int value) {
+    public void put(int key, int value) {
+        if (capacity == 0)
+            return;
         if (!keyValue.containsKey(key)) {
             //eviction code
             if (keyValue.size() == capacity) {
-                LinkedHashMap<Integer, Integer> toEvictDequeue = freqMap.get(minFreq);
-                Iterator<Integer> keysIt = toEvictDequeue.keySet().iterator();
-                Integer toEvict = keysIt.next();
-                keysIt.remove();
+                Map<Integer, Integer> toEvictDequeue = getKey(minFreq);
+                Integer toEvict = toEvictDequeue.keySet().iterator().next();
+                toEvictDequeue.remove(toEvict);
                 if (toEvictDequeue.isEmpty()) {
                     freqMap.remove(minFreq);
                 }
@@ -56,22 +56,26 @@ public class LFUCache {
             }
             Node valNode = new Node(value, 1);
             keyValue.put(key, valNode);
-            freqMap.get(1).put(key, key);
+            getKey(1).put(key, key);
             minFreq = 1;
+        } else {
+            Node nodeVal = keyValue.get(key);
+            Map<Integer, Integer> deq = getKey(nodeVal.freq);
+            deq.remove(key);
+            if (deq.isEmpty()) {
+                freqMap.remove(nodeVal.freq);
+                if (minFreq == nodeVal.freq)
+                    minFreq += 1;
+            }
+            nodeVal.freq += 1;
+            nodeVal.val = value;
+            getKey(nodeVal.freq).put(key, key);
         }
     }
 
-    class DefaultDict extends HashMap<Integer, LinkedHashMap<Integer, Integer>> {
-
-        @Override
-        public LinkedHashMap<Integer, Integer> get(Object key) {
-            LinkedHashMap<Integer, Integer> returnValue = super.get(key);
-            if (returnValue == null) {
-                returnValue = new LinkedHashMap();
-                this.put((Integer) key, returnValue);
-            }
-            return returnValue;
-        }
+    private Map<Integer, Integer> getKey(int key) {
+        freqMap.putIfAbsent(key, new LinkedHashMap());
+        return freqMap.get(key);
     }
 
     class Node {
@@ -86,27 +90,27 @@ public class LFUCache {
 
     public static void main(String[] args) {
         LFUCache obj = new LFUCache(4);
-        obj.set(1, 1);
-        obj.set(2, 2);
-        obj.set(3, 3);
-        obj.set(4, 4);//1-0,2-0,3-0,4-0
-        obj.set(5, 5);//5-0,2-0,3-0,4-0
+        obj.put(1, 1);
+        obj.put(2, 2);
+        obj.put(3, 3);
+        obj.put(4, 4);//1-0,2-0,3-0,4-0
+        obj.put(5, 5);//5-0,2-0,3-0,4-0
         System.out.print(obj.get(5)+", ");//5-1,2-0,3-0,4-0
         System.out.print(obj.get(1)+", ");//same, get=-1
         obj.get(2);//5-1,2-1,3-0,4-0
         obj.get(2);//5-1,2-2,3-0,4-0
         obj.get(4);//5-1,2-2,3-0,4-1
-        obj.set(6, 6);//5-1,2-2,6-0,4-1
+        obj.put(6, 6);//5-1,2-2,6-0,4-1
         System.out.print(obj.get(2)+", ");//5-1,2-3,6-0,4-1
         System.out.print(obj.get(3)+", ");//5-1,2-3,6-0,4-1 - same
         System.out.print(obj.get(5)+", ");//5-2,2-3,6-0,4-1
         obj.get(6);//5-2,2-3,6-1,4-1
         obj.get(6);//5-2,2-3,6-2,4-1
-        obj.set(7, 7);//5-2,2-3,6-2,7-0
+        obj.put(7, 7);//5-2,2-3,6-2,7-0
         System.out.print(obj.get(4)+", ");//5-2,2-3,6-2,7-0 - same
         System.out.print(obj.get(7)+", ");//5-2,2-3,6-2,7-1
         obj.get(7);//5-2,2-3,6-2,7-2
-        obj.set(8, 8);
+        obj.put(8, 8);
         System.out.print(obj.get(5)+", ");
         System.out.print(obj.get(2)+", ");
         System.out.print(obj.get(6)+", ");
@@ -115,15 +119,15 @@ public class LFUCache {
 
         System.out.println("\n --- next test case -----");
         obj = new LFUCache(3);
-        obj.set(1, 1);
-        obj.set(2, 2);
-        obj.set(3, 3);
+        obj.put(1, 1);
+        obj.put(2, 2);
+        obj.put(3, 3);
         obj.get(2);//2-1
         obj.get(1);//1-1
         obj.get(1);//1-2
         obj.get(1);//1-3
         obj.get(2);//2-2
-        obj.set(4, 4);
+        obj.put(4, 4);
         obj.get(2);
         System.out.print(obj.get(1)+", ");
         System.out.print(obj.get(2)+", ");
