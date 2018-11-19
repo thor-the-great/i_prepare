84aabe5886a48bc5ad810aa85c13e846975c20bb
Unknown <gthor10@gmail.com>
2018-11-19 09:41:56 -0800

    daily problems
    - singleton with 2 instances

---
+++ daily_problems/src/singleton_2inst/Singleton.java
@@ -0,0 +1,38 @@
+package singleton_2inst;
+
+import com.sun.deploy.util.SyncAccess;
+
+import java.util.stream.IntStream;
+
+public class Singleton {
+    private static Singleton instanceOne;
+    private static Singleton instanceTwo;
+    private static boolean instanceFlagOne = false;
+    String name;
+
+    private Singleton(String name) {
+        this.name = name;
+    }
+
+    public static Singleton getInstance() {
+        instanceFlagOne = !instanceFlagOne;
+        if (instanceFlagOne) {
+            if (instanceOne == null)
+                instanceOne = new Singleton("one");
+            return instanceOne;
+        }
+        else {
+            if (instanceTwo == null)
+                instanceTwo = new Singleton("two");
+            return instanceTwo;
+        }
+    }
+
+    public static void main(String[] args) {
+        IntStream.range(0, 20).forEach(i->{
+            System.out.print("it # " + i);
+            Singleton s = Singleton.getInstance();
+            System.out.print( " " + s.name +"\n");
+        });
+    }
+}
