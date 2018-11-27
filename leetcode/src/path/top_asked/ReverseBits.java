197abae8454651461e0fcf3c14778c8a2bf23e50
Unknown <gthor10@gmail.com>
2018-11-26 21:49:25 -0800

    leetcode - reverse bits corrected (unsigned int)

--- leetcode/src/path/top_asked/ReverseBits.java
+++ leetcode/src/path/top_asked/ReverseBits.java
@@ -2,6 +2,7 @@ package path.top_asked;
 
 public class ReverseBits {
     public int reverseBits(int n) {
+        long lN = n;
         int l = 16, r = 15;
         long res = 0;
         int c = 0;
@@ -9,10 +10,10 @@ public class ReverseBits {
             c++;
             long lMask = 1<<l;
             long rMask = 1<<r;
-            long lVal = n & lMask;
+            int lVal = (int) (lN & lMask);
             long rVal = n & rMask;
             res = res | (rVal << (2*c - 1));
-            res = res | (lVal >> (2*c - 1));
+            res |= (long)((lVal >>> (2*c - 1)));
             r--;
             l++;
         }
@@ -22,11 +23,14 @@ public class ReverseBits {
     public static void main(String[] args) {
         ReverseBits obj = new ReverseBits();
         int num = 0b110100001;
-        int ans = obj.reverseBits(num);
+        int ans = 0;
+        ans = obj.reverseBits(num);
         System.out.println(Integer.toBinaryString(num));
         System.out.println(Integer.toBinaryString(ans));
 
-        num = 2147483648;
+        num =   0b10000000000000000000000000000000;
         ans = obj.reverseBits(num);
+        System.out.println(Integer.toBinaryString(num));
+        System.out.println(Integer.toBinaryString(ans));
     }
 }
