635e73d4104d0447e8e5beea824ca69e0c40361e
Unknown <gthor10@gmail.com>
2018-12-10 21:38:43 -0800

    leetcode
    - generate parenthesis

---
+++ leetcode/src/random_problems/GenerateParentheses.java
@@ -0,0 +1,47 @@
+package random_problems;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 22. Generate Parentheses
+ * Medium
+ * 
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ *
+ * For example, given n = 3, a solution set is:
+ *
+ * [
+ *   "((()))",
+ *   "(()())",
+ *   "(())()",
+ *   "()(())",
+ *   "()()()"
+ * ]
+ *
+ */
+public class GenerateParentheses {
+
+    List<String> res;
+    int n;
+
+    public List<String> generateParenthesis(int n) {
+        res = new LinkedList();
+        this.n = n;
+        helper("", 0, 0);
+        return res;
+    }
+
+    void helper(String s, int open, int close) {
+        if (s.length() == 2 * n) {
+            res.add(s);
+            return;
+        }
+
+        if (open < n)
+            helper(s + "(", open + 1, close);
+        if (close < open)
+            helper(s + ")", open, close + 1);
+    }
+
+}
