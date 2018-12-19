013523bb0289660a9152154de65bfc9176caf4d7
Unknown <gthor10@gmail.com>
2018-12-18 21:40:58 -0800

    leetcode
    - container with max water

---
+++ leetcode/src/random_problems/ContainerWithMostWater.java
@@ -0,0 +1,54 @@
+package random_problems;
+
+/**
+ * 11. Container With Most Water
+ * Medium
+ * Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical
+ * lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together
+ * with x-axis forms a container, such that the container contains the most water.
+ *
+ * Note: You may not slant the container and n is at least 2.
+ *
+ *
+ *
+ *
+ *
+ * The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water
+ * (blue section) the container can contain is 49.
+ *
+ *
+ *
+ * Example:
+ *
+ * Input: [1,8,6,2,5,4,8,3,7]
+ * Output: 49
+ */
+public class ContainerWithMostWater {
+
+    /**
+     * Idea - use two pointer from opposite edges of our graph, start moving one by one calculating area
+     * @param height
+     * @return
+     */
+    public int maxArea(int[] height) {
+        int N = height.length;
+        if (N <=  1)
+            return 0;
+        int l = 0, r = N - 1;
+        int res = 0;
+        while (l < r) {
+            res = Math.max(res, (r - l) * Math.min(height[l], height[r]));
+            if (height[l] > height[r])
+                r--;
+            else
+                l++;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] containers = new int[] {1,8,6,2,5,4,8,3,7};
+        ContainerWithMostWater obj = new ContainerWithMostWater();
+        System.out.println(obj.maxArea(containers));
+    }
+}
