59d0201dd1179955de96156763f8fb772a6706a5
Unknown <gthor10@gmail.com>
2018-12-19 17:57:11 -0800

    concurrency - latches

---
+++ concurrency/src/Latches.java
@@ -0,0 +1,54 @@
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Latches {
+
+    void runTasks(int countOfThread) {
+        Random rand = new Random();
+        CountDownLatch startCounter = new CountDownLatch(1);
+        CountDownLatch endCounter = new CountDownLatch(countOfThread);
+        AtomicInteger res = new AtomicInteger(0);
+        ExecutorService executor = Executors.newFixedThreadPool(countOfThread);
+        for (int i = 0; i < countOfThread; i++) {
+            Runnable task = () -> {
+                try {
+                    startCounter.await();
+                    String s = "ddfgdfgdfgdfghjh";
+                    int iter = 5_000_000 + rand.nextInt(300);
+                    for (int j = 0; j < iter; j++) {
+                        int k = j * j;
+                        for (int l = 0; l < s.length(); l++) {
+                            String b = s.substring(0, l + 1);
+                        }
+                    }
+                    res.incrementAndGet();
+                    endCounter.countDown();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            };
+            executor.execute(task);
+        }
+        long start = System.currentTimeMillis();
+        startCounter.countDown();
+        try {
+            endCounter.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        long runTime = System.currentTimeMillis() - start;
+        System.out.println("Threads finished in ms " + runTime +". Result is " + res.get());
+        executor.shutdown();
+        while(!executor.isTerminated()) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Latches obj = new Latches();
+        obj.runTasks(5);
+    }
+}
