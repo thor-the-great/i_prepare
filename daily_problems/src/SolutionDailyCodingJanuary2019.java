794afc56f9a3bdab56c5075ad398275467189569
Unknown <gthor10@gmail.com>
2019-01-08 09:43:44 -0800

    daily problems
    - busiest time of the day

--- daily_problems/src/SolutionDailyCodingJanuary2019.java
+++ daily_problems/src/SolutionDailyCodingJanuary2019.java
@@ -63,6 +63,53 @@ public class SolutionDailyCodingJanuary2019 {
         }
     }
 
+    /**
+     * You are given a list of data entries that represent entries and exits of groups of people into a building. An
+     * entry looks like this:
+     *
+     * {"timestamp": 1526579928, count: 3, "type": "enter"}
+     *
+     * This means 3 people entered the building. An exit looks like this:
+     *
+     * {"timestamp": 1526580382, count: 2, "type": "exit"}
+     *
+     * This means that 2 people exited the building. timestamp is in Unix time.
+     *
+     * Find the busiest period in the building, that is, the time with the most people in the building. Return it as a
+     * pair of (start, end) timestamps. You can assume the building always starts off and ends up empty, i.e. with 0
+     * people inside.
+     * @param entries
+     * @return
+     */
+    int[] busiestTime(List<int[]> entries) {
+        //each entry is in form [time, numOfPeople, type]
+        //type is 1 - enter, 2 - exit
+        int[] res = new int[] {-1, -1};
+        List<int[]> sortedIntervals = new ArrayList<>();
+        for (int[] entry : entries) {
+            int idx = entry[1];
+            if (entry[2] == 2)
+                idx = -idx;
+            int[] e = new int[] {entry[0], idx};
+            sortedIntervals.add(e);
+        }
+        sortedIntervals.sort(Comparator.comparingInt(e->e[0]));
+        int max = 0;
+        int numOfPeople = 0;
+        for (int[] e : sortedIntervals) {
+            if (max == numOfPeople && e[1] < 0) {
+                res[1] = e[0];
+            }
+            numOfPeople += e[1];
+            if (numOfPeople >= max) {
+                max = numOfPeople;
+                res[0] = e[0];
+            }
+        }
+        return res;
+    }
+
+
     public static void main(String[] args) {
         SolutionDailyCodingJanuary2019 obj = new SolutionDailyCodingJanuary2019();
 
@@ -72,5 +119,23 @@ public class SolutionDailyCodingJanuary2019 {
         arr = new int[] {5,2,6,1};
         smallers = obj.countSmaller(arr);
         System.out.print(StringUtils.listToString(smallers));//[2, 1, 1, 0]
+
+        System.out.println("\n---- count busiest time in the building ----");
+        //each entry is in form [time, numOfPeople, type], type is 1 - enter, 2 - exit
+        List<int[]> entries;
+        entries = new LinkedList<>();
+        entries.add(new int[] {1, 2, 1});
+        entries.add(new int[] {3, 3, 1});
+        entries.add(new int[] {4, 1, 2});
+        entries.add(new int[] {5, 1, 1});
+        entries.add(new int[] {8, 3, 1});
+        entries.add(new int[] {20, 8, 2});
+        entries.add(new int[] {25, 4, 1});
+        entries.add(new int[] {28, 4, 1});
+        entries.add(new int[] {40, 5, 2});
+        entries.add(new int[] {45, 3, 2});
+
+        int[] time = obj.busiestTime(entries);
+        System.out.println("[ " + time[0] + ", " + time[1] + " ]");
     }
 }
