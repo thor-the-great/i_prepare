4ecf36144d563b35886cda2b49e7da7ec6534ef4
thor-the-great <thor@PALN34285434A.amer.global.corp.sap>
2019-02-20 11:07:22 -0800

    daily problems - bottom view of binary tree

--- daily_problems/src/SolutionDailyCodingFebruary2019.java
+++ daily_problems/src/SolutionDailyCodingFebruary2019.java
@@ -242,6 +242,75 @@ public class SolutionDailyCodingFebruary2019 {
 
         return res;
     }
+	
+	int dist = Integer.MAX_VALUE;
+
+	public int horizontalDistanceOfBinaryTree(TreeNode root, int v) {
+        dist = Integer.MAX_VALUE;
+		helper(root, v, 0);
+		return dist;
+	}
+
+	private void helper(TreeNode n, int v, int d) {
+		if (n == null || dist != Integer.MAX_VALUE)
+			return;
+
+		if (n.val == v)
+			dist = d;
+
+		helper(n.left, v, d - 1);
+		helper(n.right, v, d + 1);
+	}
+
+    /**
+     * This problem was asked by Yelp.
+     *
+     * The horizontal distance of a binary tree node describes how far left or right the node will be when the tree is
+     * printed out.
+     *
+     * More rigorously, we can define it as follows:
+     *
+     * The horizontal distance of the root is 0.
+     * The horizontal distance of a left child is hd(parent) - 1.
+     * The horizontal distance of a right child is hd(parent) + 1.
+     * For example, for the following tree, hd(1) = -2, and hd(6) = 0.
+     *
+     *              5
+     *           /     \
+     *         3         7
+     *       /  \      /   \
+     *     1     4    6     9
+     *    /                /
+     *   0                8
+     * The bottom view of a tree, then, consists of the lowest node at each horizontal distance. If there are two
+     * nodes at the same depth and horizontal distance, either is acceptable.
+     *
+     * For this tree, for example, the bottom view could be [0, 1, 3, 6, 8, 9].
+     *
+     * Given the root to a binary tree, return its bottom view.
+     *
+     * @param root
+     * @return
+     */
+	public List<Integer> bottomViewOfBinaryTree(TreeNode root) {
+        TreeMap<Integer, Integer> m = new TreeMap<>();
+        bottomViewHelper(m, root, 0);
+        List<Integer> res = new ArrayList<>();
+        for (int d : m.keySet()) {
+            res.add(m.get(d));
+        }
+        return res;
+    }
+
+    void bottomViewHelper(TreeMap<Integer, Integer> m, TreeNode n, int d) {
+	    if (n != null)
+	        m.put(d, n.val);
+	    else
+	        return;
+
+        bottomViewHelper(m, n.left, d - 1);
+        bottomViewHelper(m, n.right, d + 1);
+    }
 
     public static void main(String[] args) {
         SolutionDailyCodingFebruary2019 obj = new SolutionDailyCodingFebruary2019();
@@ -301,7 +370,24 @@ public class SolutionDailyCodingFebruary2019 {
 
         System.out.println("--- longest consecutive run of 1s in its binary representation ---");
         System.out.println(obj.longestConsequtiveSequenceOfOnesBitwise(156)); //3
-
         System.out.println(obj.longestConsequtiveSequenceOfOnesBitwise(222)); //4
+
+		System.out.println("--- horizontal distance for node  ---");
+        TreeNode binaryTree1 = new TreeNode(5,
+                new TreeNode(3,
+                        new TreeNode(1,
+                                new TreeNode(0), null),
+                        new TreeNode(4)),
+                new TreeNode(7,
+                        new TreeNode(6),
+                        new TreeNode(9,
+                                new TreeNode(8), null))
+        );
+        System.out.println(obj.horizontalDistanceOfBinaryTree(binaryTree1, 7));//1
+        System.out.println(obj.horizontalDistanceOfBinaryTree(binaryTree1, 1));//-2
+        System.out.println(obj.horizontalDistanceOfBinaryTree(binaryTree1, 6));//0
+
+        System.out.println("--- bottom view of a tree ---");
+        System.out.println(obj.bottomViewOfBinaryTree(binaryTree1));
     }
 }
