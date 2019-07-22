c615ff175ae227ae9f70516685260ec926902aa6
Martin <g_martin@zoho.com>
2019-07-21 23:15:53 -0700

    more problems - leetcode and firecode

---
+++ leetcode/src/random_problems/NumberOfEquvivalentDominoPairs.java
@@ -0,0 +1,57 @@
+package random_problems;
+
+/**
+ * 1128. Number of Equivalent Domino Pairs
+ * Easy
+ *
+ * Given a list of dominoes, dominoes[i] = [a, b] is equivalent to dominoes[j] = [c, d] if and only if either
+ * (a==c and b==d), or (a==d and b==c) - that is, one domino can be rotated to be equal to another domino.
+ *
+ * Return the number of pairs (i, j) for which 0 <= i < j < dominoes.length, and dominoes[i] is equivalent to dominoes[j].
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: dominoes = [[1,2],[2,1],[3,4],[5,6]]
+ * Output: 1
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= dominoes.length <= 40000
+ * 1 <= dominoes[i][j] <= 9
+ */
+public class NumberOfEquvivalentDominoPairs {
+
+    /**
+     * Iterate over the array of dominoes. Make it identical by fliping smaller to be the first. Save number of times
+     * we've seen the pair. Of each time > 1 increment result by res - 1
+     * @param dominoes
+     * @return
+     */
+    public int numEquivDominoPairs(int[][] dominoes) {
+        int[] counts = new int[100];
+
+        int res = 0;
+        //check every pair of dominoes
+        for (int[] d : dominoes) {
+            //get upper and down number, make upper always smaller
+            int up = d[0] - 1;
+            int down = d[1] - 1;
+            if (down < up) {
+                up = d[1] - 1;
+                down = d[0] - 1;
+            }
+            int idx = up * 10 + down;
+            //increment number of times we've seen exactly this pair
+            counts[idx]++;
+            //if more than once
+            if (counts[idx] > 1)
+                //increment result, every next same pair will contribute on 1 more than a previous one
+                res+=counts[idx] - 1;
+        }
+
+        return res;
+    }
+}
