510fb0789eafe3c135cd3a3c5e85ecf710b67d8f
I057833 <martin.gaievski@sap.com>
2019-08-13 14:15:02 -0700

    leetcode, DP problems
    - coin change - 1st and 2nd
    - number of combination for dice

---
+++ leetcode/src/random_problems/CoinChange2.java
@@ -0,0 +1,66 @@
+package random_problems;
+
+/**
+ * 518. Coin Change 2
+ * Medium
+ *
+ * You are given coins of different denominations and a total amount of money. Write a function
+ * to compute the number of combinations that make up that amount. You may assume that you have
+ * infinite number of each kind of coin.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: amount = 5, coins = [1, 2, 5]
+ * Output: 4
+ * Explanation: there are four ways to make up the amount:
+ * 5=5
+ * 5=2+2+1
+ * 5=2+1+1+1
+ * 5=1+1+1+1+1
+ * Example 2:
+ *
+ * Input: amount = 3, coins = [2]
+ * Output: 0
+ * Explanation: the amount of 3 cannot be made up just with coins of 2.
+ * Example 3:
+ *
+ * Input: amount = 10, coins = [10]
+ * Output: 1
+ *
+ *
+ * Note:
+ *
+ * You can assume that
+ *
+ * 0 <= amount <= 5000
+ * 1 <= coin <= 5000
+ * the number of coins is less than 500
+ * the answer is guaranteed to fit into signed 32-bit integer
+ */
+public class CoinChange2 {
+
+  /**
+   * Similar to knapsack problem. Construct the matrix of [amount][coins]. Init [0][0] = 1. Now go
+   * over and if the amount >= then a current coin - get the previous coin result for this amount
+   * and add amount for this coin for amount - coin.
+   * Finally result will be in lower right cell.
+   * @param amount
+   * @param coins
+   * @return
+   */
+  public int change(int amount, int[] coins) {
+    int[][] dp = new int[coins.length + 1][amount + 1];
+    dp[0][0] = 1;
+
+    for (int c = 1; c <= coins.length; c++) {
+      dp[c][0] = 1;
+      for (int am = 1; am <= amount; am++) {
+        dp[c][am] = dp[c - 1][am] + (am >= coins[c - 1] ? dp[c][am - coins[c - 1]] : 0);
+      }
+    }
+
+    return dp[coins.length][amount];
+  }
+}
