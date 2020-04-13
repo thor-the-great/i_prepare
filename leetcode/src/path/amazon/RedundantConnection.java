285021aa0b56e5611a499245cb2ad63d0c8222ba
Martin Gaievski <martin.gaievski@sap.com>
2020-04-13 10:44:52 -0700

    More problems

--- leetcode/src/path/amazon/RedundantConnection.java
+++ leetcode/src/path/amazon/RedundantConnection.java
@@ -1,5 +1,10 @@
 package path.amazon;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Stack;
 import java.util.stream.IntStream;
 
 public class RedundantConnection {
@@ -16,6 +21,49 @@ public class RedundantConnection {
             if (!unFind.union(edge[0], edge[1]))
                 return edge;
         }
+        return new int[2];
+    }
+
+    /**
+     * Form graph as array of adjacent nodes. Iterate over edges, check if one node from edge is
+     * reachable starting from another node by doing DFS. If so - this node makes a cycle and can
+     * be removed. Otherwise add the edge to graph (only after check)
+     *
+     * O(N^2) time, O(N) space
+     * @param edges
+     * @return
+     */
+    public int[] findRedundantConnectionGraph(int[][] edges) {
+        int N = edges.length;
+
+        List<Integer>[] g = new ArrayList[N + 1];
+        for (int i = 0; i <= N; i++)
+            g[i] = new ArrayList();
+        Set<Integer> seen = new HashSet();
+        for (int[] edge : edges) {
+            seen.clear();
+            Stack<Integer> stack = new Stack();
+            stack.push(edge[0]);
+            boolean reachable = false;
+            while (!stack.isEmpty()) {
+                int next = stack.pop();
+                if (seen.contains(next)) continue;
+                if (next == edge[1]) {
+                    reachable = true;
+                    break;
+                }
+                seen.add(next);
+                for (int adj : g[next]) {
+                    stack.push(adj);
+                }
+            }
+            if (reachable) {
+                return edge;
+            }
+            g[edge[0]].add(edge[1]);
+            g[edge[1]].add(edge[0]);
+        }
+
         return new int[2];
     }
 
