2894ace75d5ca1367a40fb71793ce85818048ca2
Odin <gthor10@gmail.com>
2020-12-12 18:23:29 -0800

    Adding Can Place Flower solution

--- leetcode/src/path/linkedin/CanPlaceFlowers.java
+++ leetcode/src/path/linkedin/CanPlaceFlowers.java
@@ -31,6 +31,25 @@ public class CanPlaceFlowers {
      * @return
      */
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if (n == 0)
+            return true;
+        int N = flowerbed.length;
+        for (int i = 0; i < N; i++) {
+            if (flowerbed[i] == 0) {
+                if ((i - 1 < 0 || flowerbed[i - 1] == 0)
+                        && (i + 1 == N || flowerbed[i + 1] == 0)) {
+                    --n;
+                    flowerbed[i] = 1;
+                }
+            }
+            if ( n == 0)
+                return true;
+        }
+        return n == 0;
+    }
+
+
+    public boolean canPlaceFlowersOld(int[] flowerbed, int n) {
         int count = 0;
         int l = 0, r;
         int N = flowerbed.length;
