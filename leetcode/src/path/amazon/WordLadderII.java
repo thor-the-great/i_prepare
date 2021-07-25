1e59ce187ad9784fc98c968cdcedb82a5fe81d4c
Odin <gthor10@gmail.com>
2021-07-24 18:31:45 -0700

    word ladder 2 clenup prev solution code

--- leetcode/src/path/amazon/WordLadderII.java
+++ leetcode/src/path/amazon/WordLadderII.java
@@ -43,9 +43,6 @@ import java.util.*;
  */
 public class WordLadderII {
 
-    Map<String, List<String>> map;
-    List<List<String>> results;
-
     /**
      * Idea: instead of counts for path we collect words that lead to this word. At the end for every word we have a
      * list of words that leads to it. Then we do backtracking with these results starting from startWord to end word,
@@ -55,82 +52,79 @@ public class WordLadderII {
      * @param wordList
      * @return
      */
-
     public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
-        results = new ArrayList();
-        if (wordList.size() == 0)
-            return results;
-        int min=Integer.MAX_VALUE;
-        Queue<String> queue= new ArrayDeque<>();
-        queue.add(beginWord);
-        map = new HashMap<>();
-
-        Map<String,Integer> ladder = new HashMap<>();
-        for (String string: wordList)
-            ladder.put(string, Integer.MAX_VALUE);
-        ladder.put(beginWord, 0);
-
+        
+        Map<String, Integer> wordPaths = new HashMap();
+        Map<String, List<String>> words = new HashMap();
+        List<List<String>> result = new ArrayList();
+        for (String word : wordList) {
+            wordPaths.put(word, Integer.MAX_VALUE);
+        }
+        wordPaths.put(beginWord, 0);
+        
+        Queue<String> q = new LinkedList();
+        q.add(beginWord);
+        
         wordList.add(endWord);
-
-        //BFS: Dijisktra search
-        char[] builder;
-        while (!queue.isEmpty()) {
-            String word = queue.poll();
-            int step = ladder.get(word) + 1;//'step' indicates how many steps are needed to travel to one word.
-            if (step>min) break;
-
-            for (int i = 0; i < word.length(); i++){
-                //StringBuilder builder = new StringBuilder(word);
-                builder = word.toCharArray();
-                for (char ch='a';  ch <= 'z'; ch++){
-                    builder[i] = ch;
-                    String new_word = new String(builder);
-                    if (ladder.containsKey(new_word)) {
-                        if (step > ladder.get(new_word))//Check if it is the shortest path to one word.
-                            continue;
-                        else if (step < ladder.get(new_word)) {
-                            queue.add(new_word);
-                            ladder.put(new_word, step);
-                        }else;// It is a KEY line. If one word already appeared in one ladder,
-                        // Do not insert the same word inside the queue twice. Otherwise it gets TLE.
-                        List<String> list;
-                        if (!map.containsKey(new_word)) {
-                            list = new ArrayList<>();
-                            map.put(new_word, list);
-                        } else{
-                            list = map.get(new_word);
-                        }
-                        list.add(word);
-
-                        if (new_word.equals(endWord))
-                            min=step;
-
-                    }//End if dict contains new_word
-                }//End:Iteration from 'a' to 'z'
-            }//End:Iteration from the first to the last
-        }//End While
-
-        //BackTracking
-        LinkedList<String> result = new LinkedList();
-        backTrace(endWord, beginWord, result);
-        return results;
+        
+        int  min = Integer.MAX_VALUE;
+        while(!q.isEmpty()) {
+            String word = q.poll();
+            int curSteps = wordPaths.get(word);
+            if (curSteps + 1 > min) {
+                break;
+            }
+            for (int i = 0; i < word.length(); i++) {
+                StringBuilder sb = new StringBuilder(word);
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    sb.setCharAt(i, ch);
+                    String oneStepWord = sb.toString();
+                    if (!wordPaths.containsKey(oneStepWord)) {
+                        continue;
+                    }
+                    
+                    if (curSteps + 1 > wordPaths.get(oneStepWord)) {
+                        continue;
+                    } else if (curSteps + 1 < wordPaths.get(oneStepWord)) {
+                        wordPaths.put(oneStepWord, curSteps + 1);
+                        q.add(oneStepWord);
+                    }
+                    
+                    if (!words.containsKey(oneStepWord)) {
+                        words.put(oneStepWord, new ArrayList());
+                    } 
+                    words.get(oneStepWord).add(word);
+                    
+                    if (oneStepWord.equals(endWord)) {
+                        min = curSteps + 1;
+                    }
+                }
+            }
+        }
+        if (min == Integer.MAX_VALUE)
+            return result;
+        backtrackToStartWord(endWord, beginWord, new ArrayList(), words, result);
+        return result;
     }
-
-    private void backTrace(String word,String start,List<String> list) {
-        if (word.equals(start)) {
-            list.add(0, start);
-            results.add(new ArrayList<>(list));
-            list.remove(0);
+    
+    void backtrackToStartWord(String cur, String start, List res, Map<String, List<String>> words, List<List<String>> result) {
+        if (cur.equals(start)) {
+            res.add(0, start);
+            result.add(new ArrayList(res));
+            res.remove(0);
             return;
         }
-        list.add(0, word);
-        if (map.containsKey(word))
-            for (String s : map.get(word))
-                backTrace(s, start, list);
-        list.remove(0);
+        
+        res.add(0, cur);
+        if (words.containsKey(cur)) {
+            for (String next : words.get(cur)) {
+                backtrackToStartWord(next, start, res, words, result);
+            }
+        }
+        res.remove(0);
     }
-
-        public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+    
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         Map<String, Integer> ladder = new HashMap();
         wordList.forEach(s->ladder.put(s, Integer.MAX_VALUE));
         if (!ladder.containsKey(endWord)) {
