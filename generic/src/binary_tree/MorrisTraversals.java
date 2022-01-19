f1111bbd7018a78484e7e66f4816822b61ca514e
Martin Gaievski <gaievski@amazon.com>
2022-01-19 13:58:57 -0800

    morris traversal - revised

--- generic/src/binary_tree/MorrisTraversals.java
+++ generic/src/binary_tree/MorrisTraversals.java
@@ -9,49 +9,82 @@ public class MorrisTraversals {
         if (root == null)
             return;
 
-        while (root != null) {
+        BSTNode node = root;
+        while (node != null) {
             //if no left children = visit this node and go to the right subtree
-            if (root.left == null) {
-                visitNode(root);
-                root = root.right;
+            if (node.left == null) {
+                visitNode(node);
+                node = node.right;
             } else {
                 //find rightmost leaf of root left node
-                BSTNode pre = root.left;
-                while (pre.right != null && pre.right != root) {
+                BSTNode pre = node.left;
+                while (pre.right != null && pre.right != node) {
                     pre = pre.right;
                 }
 
                 //pre.right has been set, go to the current node
-                if (pre.right == root) {
-                    //this means current node pointer by the left right most child
-                    //left substree has been visited
+                if (pre.right == node) {
+                    //this means the left most node's right points to the current node, so
+                    //we have reached the temporary link that we've established before in else condition
+                    //and left substree has been already visited
                     pre.right = null;
-                    visitNode(root);
-                    root = root.right;
+                    visitNode(node);
+                    node = node.right;
                 } else {
-                    //first time visit pre node, make its right chil point to the
+                    //first time visit pre node, make its right child point to the
                     //current node (save the link, will remove it later)
-                    pre.right = root;
-                    root = root.left;
+                    pre.right = node;
+                    node = node.left;
                 }
             }
         }
     }
+
+    void inorderTraversal2(BSTNode root) {
+        if (root == null) {
+            return;
+        }
+
+        BSTNode cur = root;
+        while (cur != null) {
+            if (cur.left == null) {
+                visitNode(cur);
+                cur = cur.right;
+            } else {
+                //looking for predecessor node - take the right most of the left child of the current node
+                BSTNode predecessor = cur.left;
+                while (predecessor.right != null && predecessor.right != cur) {
+                    predecessor = predecessor.right;
+                }
+                //checking if we stoped at the the current node, if :
+                // - yes : we have visited this path in the past, need to break the temporary link
+                // - no  : we haven't visited the path, need to build temporary link from predecessor to the current node to be able to come back later
+                if (predecessor.right == cur) {
+                    predecessor.right = null;
+                    visitNode(cur);
+                    cur = cur.right;
+                } else {
+                    predecessor.right = cur;
+                    cur = cur.left;
+                }
+            }
+        }
+     }
 
     private void visitNode(BSTNode node) {
         System.out.print(node + ", ");
     }
 
     public static void main(String[] args) {
-        BSTNode root3 = new BSTNode(1,
+        BSTNode root = new BSTNode(4,
                 new BSTNode(2,
-                        new BSTNode(4,null,null),
-                        new BSTNode(5,null,null)),
-                new BSTNode(3,
-                        new BSTNode(6, null, null),
+                        new BSTNode(1,null,null),
+                        new BSTNode(3,null,null)),
+                new BSTNode(6,
+                        new BSTNode(5, null, null),
                         null));
 
         MorrisTraversals obj = new MorrisTraversals();
-        obj.inorderTraversal(root3);
+        obj.inorderTraversal2(root);
     }
 }
